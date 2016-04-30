e8ac288f81387817bd2901b71eef7541df9b0c04
zry731134543 <731134543@qq.com>
2016-04-30 20:51:55 +0800

    ife

--- ife/js/Clip.js
+++ ife/js/Clip.js
@@ -1,14 +1,16 @@
 var Clip=function(){
-	var div=arguments[1];
-	console.log(div.id);
-	check_img(arguments[0]);
-	switch((arguments[0].length-1)/2){
-		case 1:one_img(arguments[0],div);break;
-		case 2:two_img(arguments[0],div);break;
-		case 3:three_img(arguments[0],div);break;
-		case 4:four_img(arguments[0],div);break;
-		case 5:five_img(arguments[0],div);break;
-		case 6:six_img(arguments[0],div);break;
+	var methods=arguments[0];
+	var div=methods[1].parentElement;
+	var width=div.offsetWidth;
+	var height=div.offsetHeight;
+	check_img(methods);
+	switch((methods.length-1)/2){
+		case 1:one_img(methods,width,height);break;
+		case 2:two_img(methods,width,height);break;
+		case 3:three_img(methods,width,height);break;
+		case 4:four_img(methods,width,height);break;
+		case 5:five_img(methods,width,height);break;
+		case 6:six_img(methods,width,height);break;
 		default:throw new Error("img max seven");		
 	}
 }
@@ -22,61 +24,63 @@ function check_img(arguments){
 	}
 }
 //一张图片布局
-function one_img(arguments,div){
+function one_img(arguments,width,height){
 	console.log("传入一张图片");
 	var img=arguments[1];
-	img.style.cssText="width:400px;height:300px;";
+	img.style.cssText="width:"+width+"px;height:"+height+"px;";
 }
 //两张图片布局
-function two_img(arguments,div){
+function two_img(arguments,width,height){
 	console.log("传入二张图片");
-	arguments[1].style.cssText="-webkit-clip-path: polygon(0 0,0 300px,133px 300px, 266px 0);";
-	arguments[3].style.cssText="-webkit-clip-path: polygon(266px 0, 133px 300px, 400px 300px, 400px 0);margin-top:-300px";
+	var clip1="polygon(0 0,0 "+height+"px,"+width/3+"px "+height+"px, "+width*2/3+"px 0);";
+	var clip2="polygon("+width*2/3+"px 0, "+width/3+"px "+height+"px, "+width+"px "+height+"px, "+width+"px 0);margin-top:-"+height+"px;";
+	arguments[1].style.cssText="-webkit-clip-path: "+clip1;
+	arguments[3].style.cssText="-webkit-clip-path: "+clip2;
 	for(var i=1;i<arguments.length;i+=2){
-		arguments[i].style.width="400px";
-		arguments[i].style.height="300px";
+		arguments[i].style.width=width+"px";
+		arguments[i].style.height=height+"px";
 	}
 }
 //三张图片布局
-function three_img(arguments,div){
+function three_img(arguments,width,height){
 	console.log("传入三张图片");
-	arguments[1].style.width="250px";
-	arguments[1].style.height="300px";
+	arguments[1].style.width=width-height/2+"px";
+	arguments[1].style.height=height+"px";
 	for(var i=3;i<arguments.length;i+=2){
-		arguments[i].style.width="150px";
-		arguments[i].style.height="150px";
+		arguments[i].style.width=height/2+"px";
+		arguments[i].style.height=height/2+"px";
 	}
 }
 //四张图片布局
-function four_img(arguments,div){
+function four_img(arguments,width,height){
 	console.log("传入四张图片");
 	for(var i=1;i<arguments.length;i+=2){
-		arguments[i].style.width="200px";
-		arguments[i].style.height="150px";
+		arguments[i].style.width=width/2+"px";
+		arguments[i].style.height=height/2+"px";
 	}
 }
 //五张图片布局
-function five_img(arguments,div){
+function five_img(arguments,width,height){
 	console.log("传入五张图片");
-	arguments[1].style.width="267px";
-	arguments[1].style.height="200px";
-	arguments[3].style.height="133px";
-	arguments[5].style.height="167px";
-	arguments[7].style.height="100px";
-	arguments[9].style.height="100px";
+	arguments[1].style.width=width*2/3+"px";
+	arguments[1].style.height=height*2/3+"px";
+	arguments[3].style.height=width/3+"px";
+	arguments[5].style.height=height-width/3+"px";
+	arguments[7].style.height=height/3+"px";
+	arguments[9].style.height=height/3+"px";
 	for(var i=3;i<arguments.length;i+=2){
-		arguments[i].style.width="133px";
+		arguments[i].style.width=width/3+"px";
 		arguments[i].style.float="right";
 	}
 }
 //六张图片布局
-function six_img(arguments,div){
+function six_img(arguments,width,height){
 	console.log("传入六张图片");
-	arguments[1].style.width="267px";
-	arguments[1].style.height="200px";
+	arguments[1].style.width=width*2/3+"px";
+	arguments[1].style.height=height*2/3+"px";
 	for(var i=3;i<arguments.length;i+=2){
-		arguments[i].style.width="133px";
-		arguments[7].style.height="100px";
+		arguments[i].style.width=width/3+"px";;
+		arguments[i].style.height=height/3+"px";
 		arguments[i].style.float="right";
 	}
 }
--- ife/js/task43.js
+++ ife/js/task43.js
@@ -1,6 +1,6 @@
-var clip1=new Clip(one.childNodes,one);
-var clip2=new Clip(tow.childNodes,tow);
-var clip3=new Clip(three.childNodes,three);
-var clip4=new Clip(four.childNodes,four);
-var clip5=new Clip(five.childNodes,five);
-var clip6=new Clip(six.childNodes,six);
+var clip1=new Clip(one.childNodes);
+var clip2=new Clip(tow.childNodes);
+var clip3=new Clip(three.childNodes);
+var clip4=new Clip(four.childNodes);
+var clip5=new Clip(five.childNodes);
+var clip6=new Clip(six.childNodes);
